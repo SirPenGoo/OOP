a52501123c8972bb3fe9dcfe64cb918da7572a52
SirPenGoo <59529037+SirPenGoo@users.noreply.github.com>
2020-10-26 20:32:11 +0100

    initial commit

---
+++ OOP20_b_01_Erlmoser_Maximilian/D1Array/D1Array.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <array>
+#include <iostream>
+using namespace std;
+
+void coutArray(array<double, 8> a) {
+	for (int i = 0; i < 4; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << a[i] << " ";
+	}
+}
+
+int main() {
+	array<double, 8> arr{ 0,0,0,0,0,0,0,0 };
+
+	for (int i = 1; i < 8; i++) {
+		arr[i] = arr[i - 1] + 0.3456789;
+	}
+	coutArray(arr);
+}

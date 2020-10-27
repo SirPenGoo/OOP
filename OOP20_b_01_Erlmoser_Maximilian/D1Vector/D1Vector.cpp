01cebcb836701848ceb91c43b36cc7e608ea3162
SirPenGoo <59529037+SirPenGoo@users.noreply.github.com>
2020-10-27 01:13:47 +0100

    Push Homework

--- OOP20_b_01_Erlmoser_Maximilian/D1Vector/D1Vector.cpp
+++ OOP20_b_01_Erlmoser_Maximilian/D1Vector/D1Vector.cpp
@@ -1,20 +1,32 @@
-// D1Vector.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void coutVector(vector<string> v, string filename) {
+	ofstream file(filename);
+	int size = v.size();
+	if (!file.is_open()) {
+		cout << "nah man";
+		return;
+	}
+	file << "Programmname=" << v[0] << "\n";
+
+	file << "Argumente=" << size-1 << "\n";
+
+	for (int i = 1; i < size; i++) {
+		file << "Argument_" << i << "=" << v[i] << "\n";
+	}
+
+	file.close();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main(int argc, const char* argv[])
+{
+	if (argc > 4)
+		return -1;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	vector<string> arguments(argv, argc + argv);
+	coutVector(arguments, "datei.txt");
+}
---
+++ OOP20_b_01_Erlmoser_Maximilian/D2VectorDet/D2VectorDet.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include <fstream>
+using namespace std;
+
+vector<vector<double>> readFile(string fpath) {
+    vector<vector<double>> matrix;
+    ifstream myfile(fpath);
+    if (myfile.is_open()) {
+            for (int i = 0; i < 3; i++) {
+                string line;
+                vector<double> tempVec;
+                for (int j = 0; j < 3; j++) {
+                    getline(myfile, line, ','); //Funktioniert nur, wenn vor \n ein Komma steht
+                    tempVec.push_back(stod(line));
+                }
+                matrix.push_back(tempVec);
+            }
+            myfile.close();
+    }
+    return matrix;
+}
+
+double calcDeterminant(vector<vector<double>> vec) {
+    double diag1, diag2, diag3; 
+    double diag4, diag5, diag6;
+
+    diag1 = vec[0][0] * vec[1][1] * vec[2][2];
+    diag2 = vec[0][1] * vec[1][2] * vec[2][0];
+    diag3 = vec[0][2] * vec[1][0] * vec[2][1];
+
+    diag4 = vec[0][2] * vec[1][1] * vec[2][0];
+    diag5 = vec[0][0] * vec[1][2] * vec[2][1];
+    diag6 = vec[0][1] * vec[1][0] * vec[2][2];
+
+    double diagPos = diag1 + diag2 + diag3;
+    double diagNeg = diag4 + diag5 + diag6;
+
+    double det = diagPos - diagNeg;
+
+    return det;
+}
+
+void coutDet(vector<vector<double>> v, double det){
+    cout << "\t" << "[" << v[0][0] << "," << v[0][1] << "," << v[0][2] << "]\n";
+    cout << "det" << "\t" << "[" << v[1][0] << "," << v[1][1] << "," << v[1][2] << "] =" << det << "\n";
+    cout << "\t" << "[" << v[2][0] << "," << v[2][1] << "," << v[2][2] << "]\n";
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 2)
+        return -1;
+
+    string path = argv[1];
+    vector<vector<double>>matrix = readFile(path);
+    coutDet(matrix, calcDeterminant(matrix));
+}
